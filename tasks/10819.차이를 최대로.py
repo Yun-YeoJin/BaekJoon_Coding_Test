fff93b86eddbc1acbcc5197e8bdd6f89c9d61d27
윤여진 <yeojin.yun@medisolution.co.kr>
2024-11-04 09:26:17 +0900

    10819.차이를 최대로

---
+++ tasks/10819.차이를 최대로.py
@@ -0,0 +1,16 @@
+from itertools import permutations
+
+N = int(input()) #배열의 길이
+numsList = list(map(int, input().split())) #정수들
+nums = []
+
+maxSum = 0
+
+for a in permutations(numsList, N):
+    sum = 0
+    for i in range(N - 1):
+        sum += abs(a[i] - a[i + 1])
+    maxSum = max(maxSum, sum)
+    
+print(maxSum)
+    
